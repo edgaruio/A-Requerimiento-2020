4525ec5ec1a6c0c99f1f11a7c83577d07ceb3847
edgaruio <59965669+edgaruio@users.noreply.github.com>
2020-07-01 15:27:44 -0500

    Add files via upload

---
+++ 03_marzo/2 Conexion_VTex/Conexion.R
@@ -0,0 +1,14 @@
+# Conexion Vtex
+
+
+# curl --request GET \
+# --url 'https://apiexamples.vtexcommercestable.com.br/api/oms/user/orders/orderId?clientEmail=%7B%7Bemail%7D%7D' \
+# --header 'accept: application/json' \
+# --header 'content-type: application/json'
+
+# library(RCurl); library(curl)
+# curl_download('https://cloud.r-project.org/CRAN_mirrors.csv', 'mirrors.csv')
+# mirros <- read.csv('mirrors.csv')
+# unlink('mirrors.csv')
+# 
+
